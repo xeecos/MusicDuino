617d33edffc68fd139dd8cec5f71e85877ab6fae
xeecos <flashindream@gmail.com>
2017-05-02 16:01:53 +0800

    update music

--- app/app.js
+++ app/app.js
@@ -18,12 +18,18 @@ var midiBuffer = [];
 var index = 0;
 SerialPort.list(function(err, ports) {
     for (var i in ports) {
-        if (ports[i].comName.indexOf("CH341") > -1) {
+        if (ports[i].comName.indexOf("CH341") > -1 || ports[i].manufacturer.indexOf("wch.cn") > -1) {
             serial = new SerialPort(ports[i].comName, {
                 baudRate: 115200
             }, function() {
                 serial.on('data', function(data) {
-                    // console.log(data.toString());
+                    console.log(data.toString());
+                    if (data.toString().split(":").length > 2) {
+                        // var dat = data.toString().split(":")
+                        // timeFile += dat[2].split("\r\n").join("") + ",";
+                        // duringFile += dat[3].split("\r\n").join("") + ",";
+                        // noteFile += (((dat[0] % 4) << 6) + (dat[1] - 20)) + ",";
+                    }
                 });
                 //SKYCITY3 LIONKING tpfo4 m0457_01 m0457_05 m0457_03 m0346_01 12289115690 12289115691 12289115692 12394614940 senbon levan
 
@@ -37,24 +43,26 @@ var prevTick = 0;
 var totalNotes = 0;
 var timeFile = "";
 var noteFile = "";
+var duringFile = "";
 var Player = new MidiPlayer.Player(function(event) {
     // console.log(event);
     // var midiMessage = MIDIMessage(event);
     // console.log("Parsed", midiMessage);
     if (event.name == 'Note on' && event.velocity > 0) {
         var note = event.noteNumber;
-        var tick = event.tick;
+        var tick = event.tick; //new Date().getTime();
         var track = event.track;
         var velocity = event.velocity;
-
+        var during = Math.max(1, 0xff & (velocity / 5));
         var delta = event.delta;
-        // if (track > 5) {
-        serial.write(new Buffer([track % 4, note, Math.max(1, 0xff & (velocity / 5)), 0xff]));
-        totalNotes++;
-        console.log(totalNotes + " - " + (tick - prevTick) + " - " + track + ":" + note);
-        // }
-        timeFile += (tick - prevTick) + ",";
-        noteFile += (((track % 4) << 6) + (note - 30)) + ",";
+        if ((tick - prevTick) >= 0) {
+            serial.write(new Buffer([track % 4, note, during, 0xff]));
+            totalNotes++;
+            console.log(totalNotes + " - " + (tick - prevTick) + " - " + track + ":" + (note - 25));
+            timeFile += (tick - prevTick) + ",";
+            duringFile += during + ",";
+            noteFile += (((track % 4) << 6) + (note - 25)) + ",";
+        }
 
         prevTick = tick;
         // Player.stop();
@@ -66,6 +74,42 @@ var Player = new MidiPlayer.Player(function(event) {
         }
     }
 });
+
+function parseTracks(events) {
+    return;
+    timeFile = "";
+    noteFile = "";
+    duringFile = "";
+    totalNotes = 0;
+    for (var i = 0; i < events.length; i++) {
+        if (events[i])
+            for (var j = 0; j < events[i].length; j++) {
+                var event = events[i][j];
+                if (event.name == 'Note on' && event.velocity > 0) {
+                    var note = event.noteNumber;
+                    var tick = event.tick; //new Date().getTime();
+                    var track = event.track;
+                    var velocity = event.velocity;
+                    var during = Math.max(1, 0xff & (velocity / 5));
+                    var delta = event.delta;
+                    if ((tick - prevTick) >= 0) {
+                        totalNotes++;
+                        console.log(totalNotes + " - " + (tick - prevTick) + " - " + track + ":" + (note - 25));
+                        timeFile += (tick - prevTick) + ",";
+                        duringFile += during + ",";
+                        noteFile += (((track % 4) << 6) + (note - 25)) + ",";
+                    }
+
+                    prevTick = tick;
+                }
+            }
+    }
+    if (timeFile.length > 40) {
+        fs.writeFileSync("./time.txt", timeFile);
+        fs.writeFileSync("./notes.txt", noteFile);
+        fs.writeFileSync("./durings.txt", duringFile);
+    }
+}
 const id = powerSaveBlocker.start('prevent-app-suspension')
 
 function createWindow() {
@@ -92,7 +136,7 @@ function createWindow() {
 
 }
 
-var list = ["SKYCITY3", "Gravity_Falls_Theme", "tpfo4", "m0457_01", "m0457_05", "m0457_03", "m0346_01", "12289115690", "12289115691", "12289115692", "12394614940", "senbon", "levan"];
+var list = ["slam", "SKYCITY3", "Gravity_Falls_Theme", "tpfo4", "m0457_01", "m0457_05", "m0457_03", "m0346_01", "12289115690", "12289115691", "12289115692", "12394614940", "senbon", "levan"];
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
@@ -111,17 +155,22 @@ ipcMain.on('ready', (event, arg) => {
     Player.stop();
     Player.loadFile(__dirname + '/../web/midi/' + list[arg.file * 1] + '.mid');
     Player.play();
+    parseTracks(Player.getTracks().events);
     timeFile = "";
     noteFile = "";
+    duringFile = "";
+    totalNotes = 0;
 });
 var index = 0;
 ipcMain.on('stop', (event, arg) => {
     Player.stop();
-    if (timeFile.length > 900) {
+    if (timeFile.length > 40) {
         fs.writeFileSync("./time.txt", timeFile);
         fs.writeFileSync("./notes.txt", noteFile);
+        fs.writeFileSync("./durings.txt", duringFile);
         timeFile = "";
         noteFile = "";
+        duringFile = "";
         totalNotes = 0;
     }
 });
